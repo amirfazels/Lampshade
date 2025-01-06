e9d855ae3af2337b6737ed15ac8b824643f50c59
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-06 15:19:01 +0330

    Create IProductCategoryRepository and Implemented Add Aplication and Application.Contracts Layer

---
+++ ShopManagement.Domain/ProductCategoryAgg/IProductCategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Domain.ProductCategoryAgg
+{
+    public interface IProductCategoryRepository
+    {
+        void Create(ProductCategory entity);
+        ProductCategory Get(long id);
+        ICollection<ProductCategory> GetAll();
+
+    }
+}

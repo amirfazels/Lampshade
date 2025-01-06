bea2893cdedd9a294ef4b90da8417cc833b238e4
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-07 03:22:05 +0330

    Add ProductCategoryApplication class and project reference
    Updated ShopManagement.Application.csproj to include a project
    reference to ShopManagement.Application.Contacts. Added a new
    ProductCategoryApplication class implementing IProductCategoryApplication
    with method stubs for Create, Edit, GetDetails, and Search operations.
    Included necessary using directives in ProductCategoryApplication.cs.

---
+++ ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,28 @@
+﻿using ShopManagement.Application.Contacts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        public void Create(CreateProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(EditProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

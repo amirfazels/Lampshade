f4fd8241b3870e2a055873e4b7cb1d0b632800fe
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-09 01:40:34 +0330

    Add project reference and configure ProductCategory mapping
    Added a project reference to `ShopManagement.Domain` in the `ShopManagement.Infrastructure.EFCore.csproj` file, establishing a dependency on the domain project. Created a new `ProductCategoryMapping` class in the `ShopManagement.Infrastructure.EFCore.Mapping` namespace to configure the `ProductCategory` entity using Entity Framework Core's fluent API. This configuration includes setting the table name, primary key, and various property constraints.

---
+++ ShopManagement.Infrastructure.EFCore/Mapping/ProductCategoryMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Mapping
+{
+    public class ProductCategoryMapping
+    {
+        public ProductCategoryMapping(EntityTypeBuilder<ProductCategory> entityTypeBuilder)
+        {
+            entityTypeBuilder.ToTable("ProductCategories");
+            entityTypeBuilder.HasKey(x => x.Id);
+            entityTypeBuilder.Property(x => x.Name).IsRequired().HasMaxLength(250);
+            entityTypeBuilder.Property(x => x.Keywords).IsRequired().HasMaxLength(80);
+            entityTypeBuilder.Property(x => x.MetaDescription).IsRequired().HasMaxLength(150);
+            entityTypeBuilder.Property(x => x.Slug).HasMaxLength(300);
+            entityTypeBuilder.Property(x => x.PictureAlt).HasMaxLength(250);
+            entityTypeBuilder.Property(x => x.PictureTitle).HasMaxLength(500);
+            entityTypeBuilder.Property(x => x.Description).HasMaxLength(500);
+            entityTypeBuilder.Property(x => x.Picture).HasMaxLength(1000);
+        }
+    }
+}

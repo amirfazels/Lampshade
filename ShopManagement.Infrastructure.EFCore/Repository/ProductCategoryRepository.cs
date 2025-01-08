508c9491a08ac86e9163705cd662cb43bc2625d1
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-09 02:05:00 +0330

    Add ProductCategoryRepository with CRUD and search methods
    Introduce ProductCategoryRepository in ShopManagement.Infrastructure.EFCore.Repository namespace.
    - Implement IProductCategoryRepository interface.
    - Add Create, Exists, Get, GetAll, GetDetails, SaveChanges, and Search methods.

---
+++ ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using ShopManagement.Application.Contacts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using System.Linq.Expressions;
+
+namespace ShopManagement.Infrastructure.EFCore.Repository
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryRepository(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ProductCategory entity)
+        {
+            _context.ProductCategories.Add(entity);
+        }
+
+        public bool Exists(Expression<Func<ProductCategory, bool>> expression)
+        {
+            return _context.ProductCategories.Any(expression);
+        }
+
+        public ProductCategory Get(long id)
+        {
+            return _context.ProductCategories.FirstOrDefault(x => x.Id == id);
+        }
+
+        public ICollection<ProductCategory> GetAll()
+        {
+            return _context.ProductCategories.ToList();
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _context.ProductCategories.Select(x => new EditProductCategory
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Slug = x.Slug
+            }).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public ICollection<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            var query = _context.ProductCategories.Select(x => new ProductCategoryViewModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Picture = x.Picture,
+                CreationDate = x.CreationDate.ToString(),
+            });
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+            return query.OrderByDescending(x => x.Id).ToList();
+        }
+    }
+}

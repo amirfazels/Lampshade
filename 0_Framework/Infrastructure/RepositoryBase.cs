14c5cf3c98832cdb540763f217a4344bacae1e3e
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-09 17:09:23 +0330

    Add EF Core, IRepository interface, and RepositoryBase class
    Added Microsoft.EntityFrameworkCore package version 9.0.0 to 0_Framework.csproj to enable EF Core functionalities. Introduced IRepository interface in _0_Framework.Domain for generic data access operations. Implemented IRepository in RepositoryBase class in _0_Framework.Infrastructure using EF Core.

---
+++ 0_Framework/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,41 @@
+﻿using _0_Framework.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace _0_Framework.Infrastructure
+{
+    public class RepositoryBase<TKey, T> : IRepository<TKey, T> where T : class
+    {
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public void Create(T entity)
+        {
+            _context.Add<T>(entity);
+        }
+
+        public bool Exists(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Any(expression);
+        }
+
+        public T Get(TKey id)
+        {
+            return _context.Find<T>(id);
+        }
+
+        public ICollection<T> Get()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}

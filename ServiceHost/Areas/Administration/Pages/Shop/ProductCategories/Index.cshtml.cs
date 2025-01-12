d25d65b9411e4f6d379bf72e63e04ca2dcaf79f1
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-13 02:52:17 +0330

    Add product category management and update dependencies
    Updated Index.cshtml to use new model and layout, added HTML for product category management interface. Modified _AdminLayout.cshtml to include new stylesheets and scripts. Created Index.cshtml.cs to define IndexModel class. Updated launchSettings.json with new profiles and settings. Updated ServiceHost.csproj with new package and project references. Updated appsettings.json with new connection string.
    
    Modified jquery.dataTables.min.css to comment out border-right-width. Updated jquery.dataTables.min.js to change pagination text to Persian. Removed styles from site.css. Removed comments from site.js. Updated ShopManagement.Configuration.csproj to target .NET 9.0 and added new package reference. Added new migration files for initial database schema. Updated ShopContextModelSnapshot.cs to reflect current model state.

---
+++ ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contacts.ProductCategory;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
+{
+    public class IndexModel : PageModel
+    {
+        public ProductCategorySearchModel SearchModel;
+        public ICollection<ProductCategoryViewModel> ProductCategories;
+        private readonly IProductCategoryApplication _productCategoryApplication;
+
+        public IndexModel(IProductCategoryApplication productCategoryApplication)
+        {
+            _productCategoryApplication = productCategoryApplication;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            ProductCategories = _productCategoryApplication.Search(searchModel);
+        }
+    }
+}

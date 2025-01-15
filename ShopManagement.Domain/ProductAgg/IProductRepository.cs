418b35e4b7ebad0ba32abebdb28aad10e4955d2e
Amir Fazel Sobhani Far <amirfazel.sf@outlook.com>
2025-01-15 15:36:30 +0330

    Update IProductRepository interface and add using directive
    The IProductRepository interface now includes a Search method that
    takes a ProductSearchModel and returns a collection of
    ProductViewModel objects. The GetDetails method has been removed
    from the interface. Additionally, a new using directive for
    ShopManagement.Application.Contacts.Product has been added to
    support the new Search method.

--- ShopManagement.Domain/ProductAgg/IProductRepository.cs
+++ ShopManagement.Domain/ProductAgg/IProductRepository.cs
@@ -1,9 +1,11 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Application.Contacts.Product;
 
 namespace ShopManagement.Domain.ProductAgg
 {
     public interface IProductRepository : IRepository<long, Product>
     {
-
+        EditProduct GetDetails(long id);
+        ICollection<ProductViewModel> Search(ProductSearchModel searchModel);
     }
 }

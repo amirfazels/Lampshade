0795317d495929cf1b6764399b78219ded4edd70
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-01-16 17:03:49 +0330

    Add ProductPictureApplication class for managing pictures
    Introduce ProductPictureApplication implementing IProductPictureApplication.
    Includes methods for creating, editing, removing, restoring, and searching
    product pictures. Ensures no duplicates on create/edit. Uses OperationResult
    for operation outcomes. Relies on IProductPictureRepository for data access.

---
+++ ShopManagement.Application/ProductPictureApplication.cs
@@ -0,0 +1,91 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contacts.ProductPicture;
+using ShopManagement.Domain.ProductPictureAgg;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ShopManagement.Application
+{
+    public class ProductPictureApplication : IProductPictureApplication
+    {
+        private readonly IProductPictureRepository _productPictureRepository;
+
+        public ProductPictureApplication(IProductPictureRepository productPictureRepository)
+        {
+            _productPictureRepository = productPictureRepository;
+        }
+
+        public OperationResult Create(CreateProductPicture command)
+        {
+            OperationResult operation = new OperationResult();
+            if (_productPictureRepository
+                .Exists(x => x.ProductId == command.ProductId && x.Picture == command.Picture))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            
+            var productPicture = new ProductPicture(
+                command.ProductId, 
+                command.Picture, 
+                command.PictureAlt, 
+                command.PictureTitle);
+            _productPictureRepository.Create(productPicture);
+            _productPictureRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public OperationResult Edit(EditProductPicture command)
+        {
+            OperationResult operation = new OperationResult();
+            var productPicture = _productPictureRepository.Get(command.Id);
+
+            if (productPicture == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            if (_productPictureRepository
+                .Exists(x => x.ProductId == command.ProductId && x.Picture == command.Picture && x.Id != command.Id))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            productPicture.Edit(
+                command.ProductId, 
+                command.Picture, 
+                command.PictureAlt, 
+                command.PictureTitle);
+            _productPictureRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public EditProductPicture GetDetails(long id)
+        {
+            return _productPictureRepository.GetDetails(id);
+        }
+
+        public OperationResult Remove(long id)
+        {
+            OperationResult operation = new OperationResult();
+            var productPicture = _productPictureRepository.Get(id);
+
+            if (productPicture == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            productPicture.Remove();
+            _productPictureRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public OperationResult Restore(long id)
+        {
+            OperationResult operation = new OperationResult();
+            var productPicture = _productPictureRepository.Get(id);
+
+            if (productPicture == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            productPicture.Restore();
+            _productPictureRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public ICollection<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel)
+        {
+            return _productPictureRepository.Search(searchModel);
+        }
+    }
+}

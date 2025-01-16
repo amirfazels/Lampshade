caf6acf20b2a5411306cb9c06735d6411a0e1f04
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-01-16 16:41:43 +0330

    Add new methods to IProductPictureRepository interface
    Updated IProductPictureRepository with GetDetails(long id) and
    Search(ProductPictureSearchModel searchModel) methods. Added
    using directive for ShopManagement.Application.Contacts.ProductPicture
    to support new methods.

--- ShopManagement.Domain/ProductPictureAgg/IProductPictureRepository.cs
+++ ShopManagement.Domain/ProductPictureAgg/IProductPictureRepository.cs
@@ -1,8 +1,11 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Application.Contacts.ProductPicture;
 
 namespace ShopManagement.Domain.ProductPictureAgg
 {
     public interface IProductPictureRepository : IRepository<long, ProductPicture>
     {
+        EditProductPicture GetDetails(long id);
+        ICollection<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel);
     }
 }

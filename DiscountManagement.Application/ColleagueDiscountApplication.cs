e19c4285e3c01db75d7b1122ae57e1daf17ca941
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-01-29 04:55:55 +0330

    Add ColleagueDiscountApplication class with interface
    Introduce ColleagueDiscountApplication class in DiscountManagement.Application namespace implementing IColleagueDiscountApplication interface. Methods Define, Edit, GetDetails, Remove, Restore, and Search throw NotImplementedException. Inject IColleagueDiscountRepository dependency via constructor. Add necessary using directives.

---
+++ DiscountManagement.Application/ColleagueDiscountApplication.cs
@@ -0,0 +1,46 @@
+﻿using _0_Framework.Application;
+using DiscountManagement.Applicatiom.Contract.ColleagueDiscount;
+using DiscountManagement.Domain.ColleagueDiscountAgg;
+
+namespace DiscountManagement.Application
+{
+    public class ColleagueDiscountApplication : IColleagueDiscountApplication
+    {
+        private readonly IColleagueDiscountRepository _colleagueDiscountRepository;
+
+        public ColleagueDiscountApplication(IColleagueDiscountRepository colleagueDiscountRepository)
+        {
+            _colleagueDiscountRepository = colleagueDiscountRepository;
+        }
+
+        public OperationResult Define(DefineColleagueDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditColleagueDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditColleagueDiscount GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Remove(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Restore(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

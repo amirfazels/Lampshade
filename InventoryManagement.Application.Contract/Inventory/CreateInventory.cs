b0f0b3d3ea8e3eb003221b328f2dc571e832849c
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-06 17:18:26 +0330

    Add validation attributes and using directives
    Added `using` directives for `_0_Framework.Application` and `System.ComponentModel.DataAnnotations`.
    Added `[Range]` attributes to `ProductId` and `UnitPrice` properties to enforce value ranges and enhance data validation.

--- InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
+++ InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
@@ -1,10 +1,14 @@
-﻿using ShopManagement.Application.Contacts.Product;
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contacts.Product;
+using System.ComponentModel.DataAnnotations;
 
 namespace InventoryManagement.Application.Contract.Inventory;
 
 public class CreateInventory
 {
+    [Range(1,long.MaxValue, ErrorMessage = ValidationMessage.IsRequired)]
     public long ProductId { get; set; }
+    [Range(1, double.MaxValue, ErrorMessage = ValidationMessage.IsRequired)]
     public double UnitPrice { get; set; }
     public ICollection<ProductViewModel> Products { get; set; }
 

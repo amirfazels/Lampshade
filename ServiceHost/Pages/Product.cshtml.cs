f32e05fb4cfb3837a73db2dcdafd97ffe436cbed
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-22 19:42:09 +0330

    Update UI conditions and add form submission handling
    Updated conditions in Index.cshtml for displaying UI elements.
    Modified Product.cshtml form to use POST method, changed button
    text to "ارسال", and added hidden fields for productId and productSlug.
    Enhanced Product.cshtml.cs to handle form submissions by adding
    ICommentApplication dependency, updating the constructor, and
    implementing OnPost method to process form data and redirect.

--- ServiceHost/Pages/Product.cshtml.cs
+++ ServiceHost/Pages/Product.cshtml.cs
@@ -1,6 +1,7 @@
 using _01_LampshadeQuery.Contracts.Product;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contacts.Comment;
 
 namespace ServiceHost.Pages
 {
@@ -8,15 +9,22 @@ namespace ServiceHost.Pages
     {
         public ProductQueryModel Product;
         private readonly IProductQuery _productQuery;
+        private readonly ICommentApplication _commentApplication;
 
-        public ProductModel(IProductQuery productQuery)
+        public ProductModel(IProductQuery productQuery, ICommentApplication commentApplication)
         {
             _productQuery = productQuery;
+            _commentApplication = commentApplication;
         }
 
         public void OnGet(string id)
         {
             Product = _productQuery.GetDetails(id);
+        }
+        public IActionResult OnPost(AddComment command, string ProductSlug)
+        {
+            _commentApplication.Add(command);
+            return RedirectToPage("/Product", new { id = ProductSlug });
         }
     }
 }

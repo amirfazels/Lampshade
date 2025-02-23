c9e114e043cc825c7807715341396ef7243e0d72
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-23 17:15:53 +0330

    Add EF Core context and new projects to solution
    Added new projects `BM.Infrastructure` and `BlogManagement.Infrastructure.EFCore` to `Lampshade.sln`. Updated solution configuration and nested project relationships. Introduced `BlogContext` class in `BlogContext.cs` with `DbSet<ArticleCategory>` and `OnModelCreating` method. Created `BlogManagement.Infrastructure.EFCore.csproj` targeting .NET 9.0 with necessary EF Core package references and a project reference to `BlogManagement.Domain`.

---
+++ BlogManagement.Infrastructure.EFCore/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogManagement.Domain.ArticleCategoryAgg;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogManagement.Infrastructure.EFCore
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<ArticleCategory> ArticleCategories { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BlogContext).Assembly);
+        }
+    }
+}

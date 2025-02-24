4dc265f219ff8315417d2fa1cf2ab198811124ca
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-24 16:45:40 +0330

    Add ArticleApplication class with unimplemented methods
    Introduce ArticleApplication class in BlogManagement.Application namespace, implementing IArticleApplication interface. Methods for creating, editing, retrieving details, and searching for articles are defined but currently throw NotImplementedException. Added necessary using directives for _0_Framework.Application and BlogManagement.Application.Contacts.Article.

---
+++ BlogManagement.Application/ArticleApplication.cs
@@ -0,0 +1,36 @@
+﻿using _0_Framework.Application;
+using BlogManagement.Application.Contacts.Article;
+using BlogManagement.Domain.ArticleAgg;
+
+namespace BlogManagement.Application
+{
+    public class ArticleApplication : IArticleApplication
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleApplication(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public OperationResult Create(CreateArticle command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditArticle command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditArticle GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleViewModel> Search(ArticleSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

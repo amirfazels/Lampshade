49f522228366b3fd17bc815ad4f2e6eb20df2819
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-24 16:47:04 +0330

    Implement Search method in ArticleApplication class
    The Search method in the ArticleApplication class has been
    implemented. Previously, it threw a NotImplementedException,
    but now it returns the result of the _articleRepository.Search
    (searchModel) method.

--- BlogManagement.Application/ArticleApplication.cs
+++ BlogManagement.Application/ArticleApplication.cs
@@ -30,7 +30,7 @@ namespace BlogManagement.Application
 
         public List<ArticleViewModel> Search(ArticleSearchModel searchModel)
         {
-            throw new NotImplementedException();
+            return _articleRepository.Search(searchModel);
         }
     }
 }

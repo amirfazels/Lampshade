c13d2e3e324ebffa9ed5a5f1b790b33bc26f87ba
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-25 22:52:54 +0330

    Add article creation page with validation
    Updated `CreateArticle.cs` to include additional validation attributes, ensuring required fields are not empty and `CategoryId` is greater than 0.
    
    Added `Create.cshtml` Razor Page to provide a UI for creating a new article, including form fields with validation messages.
    
    Added `Create.cshtml.cs` to handle backend logic for the `Create.cshtml` page, including form submission and redirection.

--- BlogManagement.Application.Contacts/Article/CreateArticle.cs
+++ BlogManagement.Application.Contacts/Article/CreateArticle.cs
@@ -1,20 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogManagement.Application.Contacts.Article
 {
     public class CreateArticle
     {
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string ShortDescription { get; set; }
+
         public string Description { get; set; }
         public IFormFile Pictrue { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string PictrueAlt { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string PictrueTitle { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string Slug { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string Keywords { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string MetaDescription { get; set; }
+
         public string CanonicalAddress { get; set; }
+
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
         public string PublishDate { get; set; }
+
+        [Range(1,long.MaxValue, ErrorMessage = ValidationMessage.IsRequired)]
         public long CategoryId { get; set; }
     }
 }

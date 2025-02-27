e2325b3c57cca667e0684337bb5cd3fd8db09390
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-27 20:59:12 +0330

    Add IArticleQuery interface with article query methods
    Introduced a new namespace `_01_LampshadeQuery.Contracts.Article`.
    Added `IArticleQuery` interface with three methods:
    - `GetArticleDetails(string slug)` returns `ArticleQueryModel`.
    - `GetLatestArticles()` returns a list of `ArticleQueryModel`.
    - `Search(string value)` returns a list of `ArticleQueryModel`.

---
+++ 01_LampshadeQuery/Contracts/Article/IArticleQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_LampshadeQuery.Contracts.Article
+{
+    public interface IArticleQuery
+    {
+        ArticleQueryModel GetArticleDetails(string slug);
+        List<ArticleQueryModel> GetLatestArticles();
+        List<ArticleQueryModel> Search(string value);
+    }
+}

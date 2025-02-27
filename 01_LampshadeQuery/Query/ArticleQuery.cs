c5c6bce51cb6f9784075f9c71a6cc84a76f12201
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-27 21:05:13 +0330

    Add BlogContext dependency to ArticleQuery
    Updated 01_LampshadeQuery.csproj to include a project reference to BlogManagement.Infrastructure.EFCore.csproj. Added a using directive for BlogManagement.Infrastructure.EFCore in ArticleQuery.cs. Introduced a private readonly field _blogContext in the ArticleQuery class and a constructor to initialize it.

--- 01_LampshadeQuery/Query/ArticleQuery.cs
+++ 01_LampshadeQuery/Query/ArticleQuery.cs
@@ -1,9 +1,17 @@
 ﻿using _01_LampshadeQuery.Contracts.Article;
+using BlogManagement.Infrastructure.EFCore;
 
 namespace _01_LampshadeQuery.Query
 {
     public class ArticleQuery : IArticleQuery
     {
+        private readonly BlogContext _blogContext;
+
+        public ArticleQuery(BlogContext blogContext)
+        {
+            _blogContext = blogContext;
+        }
+
         public ArticleQueryModel GetArticleDetails(string slug)
         {
             throw new NotImplementedException();

8f4476dc9428ebf4c59fa313ae4314c173aaf937
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-02-27 21:02:11 +0330

    Add ArticleQuery class with placeholder methods
    Introduce ArticleQuery class in _01_LampshadeQuery.Query namespace.
    Implements IArticleQuery interface. Defines methods GetArticleDetails,
    GetLatestArticles, and Search, all throwing NotImplementedException.

---
+++ 01_LampshadeQuery/Query/ArticleQuery.cs
@@ -0,0 +1,22 @@
+﻿using _01_LampshadeQuery.Contracts.Article;
+
+namespace _01_LampshadeQuery.Query
+{
+    public class ArticleQuery : IArticleQuery
+    {
+        public ArticleQueryModel GetArticleDetails(string slug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleQueryModel> GetLatestArticles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleQueryModel> Search(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

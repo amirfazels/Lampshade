97f0b9e243ba19bc2ca092aba43d7d7e8a48f594
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-08 22:32:13 +0330

    Add AccountManagement.Application project to solution
    Introduce new AccountManagement.Application project to Lampshade.sln.
    Add AccountApplication class implementing IAccountApplication with
    unimplemented methods. Configure project with .NET 9.0, implicit usings,
    nullable reference types, and references to related projects.

---
+++ AccountManagement.Application/AccountApplication.cs
@@ -0,0 +1,41 @@
+﻿using _0_Framework.Application;
+using AccountManagement.Application.Contracts.Account;
+using AccountManagement.Domain.AccountAgg;
+
+namespace AccountManagement.Application
+{
+    public class AccountApplication : IAccountApplication
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountApplication(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public OperationResult ChangePassword(ChangePassword command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Create(CreateAccount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditAccount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditAccount GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AccountViewModel> Search(AccountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

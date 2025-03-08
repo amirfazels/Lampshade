4fe0e758e668d71e3db474fb39ec2680d8e0141b
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-08 19:22:00 +0330

    Add EFCore project and update solution configuration
    Added AccountManagement.Infrastructure.EFCore project to the solution. Updated Lampshade.sln to include new projects and configurations. Added AccountContext.cs with DbContext implementation. Created AccountManagement.Infrastructure.EFCore.csproj targeting .NET 9.0 with necessary EFCore NuGet packages and project references.

---
+++ AccountManagement.Infrastructure.EFCore/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using AccountManagement.Domain.AccountAgg;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountManagement.Infrastructure.EFCore
+{
+    public class AccountContext : DbContext
+    {
+        DbSet<Account> Accounts { get; set; }
+
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options) { }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}

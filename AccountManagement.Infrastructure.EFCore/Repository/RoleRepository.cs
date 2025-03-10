9c8dc6316a8d2b0e0efdd613b9bc751c2953288b
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-10 21:26:23 +0330

    Add RoleRepository class with unimplemented methods
    Introduce RoleRepository in AccountManagement.Infrastructure.EFCore.Repository.
    Inherit from RepositoryBase and implement IRoleRepository.
    Add constructor to initialize _roleContext with AccountContext.
    Include using directives for necessary namespaces.

---
+++ AccountManagement.Infrastructure.EFCore/Repository/RoleRepository.cs
@@ -0,0 +1,26 @@
+﻿using _0_Framework.Infrastructure;
+using AccountManagement.Application.Contracts.Role;
+using AccountManagement.Domain.RoleAgg;
+
+namespace AccountManagement.Infrastructure.EFCore.Repository
+{
+    public class RoleRepository : RepositoryBase<long, Role>, IRoleRepository
+    {
+        public AccountContext _roleContext;
+
+        public RoleRepository(AccountContext roleContext) : base(roleContext)
+        {
+            _roleContext = roleContext;
+        }
+
+        public EditRole GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RoleViewModel> Search(RoleSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

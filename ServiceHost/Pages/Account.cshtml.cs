ca114516127c69eebd20c95cf148b815f0004853
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-14 00:54:41 +0330

    Add login handling to Account page
    Modified Account.cshtml to include asp-page-handler for form submission. Updated Account.cshtml.cs to add _accountApplication field, TempData property for messages, constructor, and OnPostLogin method to handle login logic and redirection based on login success.

--- ServiceHost/Pages/Account.cshtml.cs
+++ ServiceHost/Pages/Account.cshtml.cs
@@ -1,3 +1,4 @@
+using AccountManagement.Application.Contracts.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,26 @@ namespace ServiceHost.Pages
 {
     public class AccountModel : PageModel
     {
+        private readonly IAccountApplication _accountApplication;
+
+        [TempData] public string Message { get; set; }
+
+        public AccountModel(IAccountApplication accountApplication)
+        {
+            _accountApplication = accountApplication;
+        }
+
         public void OnGet()
         {
+        }
+        public IActionResult OnPostLogin(Login command)
+        {
+            var result = _accountApplication.Login(command);
+            if (result.IsSuccedded)
+                return RedirectToPage("/Index");
+
+            Message = result.Message;
+            return RedirectToPage("/Login");
         }
     }
 }

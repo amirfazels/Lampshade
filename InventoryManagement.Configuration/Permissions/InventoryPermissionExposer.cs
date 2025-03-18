a5057750afaedc6cd897749dd74df5e0a959380f
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-18 04:57:36 +0330

    Add InventoryPermissionExposer class for inventory permissions
    Introduce InventoryPermissionExposer implementing IPermissionExposer.
    This class exposes inventory-related permissions: ListInventory,
    SearchInventory, CreateInventory, and EditInventory, each represented
    by a PermissionDto object with a unique ID and name.

---
+++ InventoryManagement.Configuration/Permissions/InventoryPermissionExposer.cs
@@ -0,0 +1,23 @@
+﻿using _0_Framework.Infrastructure;
+
+namespace InventoryManagement.Configuration.Permissions
+{
+    public class InventoryPermissionExposer : IPermissionExposer
+    {
+        public Dictionary<string, List<PermissionDto>> Expose()
+        {
+            return new Dictionary<string, List<PermissionDto>>
+            {
+                {
+                    "Inventory", new List<PermissionDto>
+                    {
+                        new PermissionDto(500, "ListInventory"),
+                        new PermissionDto(501, "SearchInventory"),
+                        new PermissionDto(502, "CreateInventory"),
+                        new PermissionDto(503, "EditInventory"),
+                    }
+                }
+            };
+        }
+    }
+}

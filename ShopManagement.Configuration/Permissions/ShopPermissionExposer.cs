600bee9162b52379653c24944366e5ad2d3c9496
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-18 04:39:38 +0330

    Add permission exposer classes and interfaces
    Introduce _0_Framework.Infrastructure namespace with IPermissionExposer and PermissionDto classes. Add ShopPermissionExposer in ShopManagement.Configuration.Permissions to implement IPermissionExposer and provide permissions for Product and ProductCategory.

---
+++ ShopManagement.Configuration/Permissions/ShopPermissionExposer.cs
@@ -0,0 +1,32 @@
+﻿using _0_Framework.Infrastructure;
+
+namespace ShopManagement.Configuration.Permissions
+{
+    public class ShopPermissionExposer : IPermissionExposer
+    {
+        public Dictionary<string, List<PermissionDto>> Expose()
+        {
+            return new Dictionary<string, List<PermissionDto>>()
+            {
+                {
+                    "Product", new List<PermissionDto>()
+                    {
+                        new PermissionDto(100, "ListProducts"),
+                        new PermissionDto(101, "SearchProducts"),
+                        new PermissionDto(102, "CreateProduct"),
+                        new PermissionDto(103, "EditProduct"),
+                    }
+                },
+                {
+                    "ProductCategory", new List<PermissionDto>()
+                    {
+                        new PermissionDto(200, "ListProductCategories"),
+                        new PermissionDto(201, "SearchProductCategories"),
+                        new PermissionDto(203, "CreateProductCategory"),
+                        new PermissionDto(202, "EditProductCategory"),
+                    }
+                },
+            };
+        }
+    }
+}

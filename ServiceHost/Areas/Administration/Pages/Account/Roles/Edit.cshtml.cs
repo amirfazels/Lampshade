e066570c73c9058631fa1e0bbcb92f255df12594
ค๓เг Ŧคzєl <amirfazel.sf@outlook.com>
2025-03-18 05:16:59 +0330

    Add permissions dropdown to Edit view and populate it
    - Modify Edit.cshtml to bind permissions to a select element
    - Add necessary using directives in Edit.cshtml.cs
    - Introduce Permissions property in EditModel class
    - Update EditModel constructor to accept IPermissionExposer
    - Populate Permissions list in OnGet method with grouped items

--- ServiceHost/Areas/Administration/Pages/Account/Roles/Edit.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Account/Roles/Edit.cshtml.cs
@@ -1,6 +1,8 @@
+using _0_Framework.Infrastructure;
 using AccountManagement.Application.Contracts.Role;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ServiceHost.Areas.Administration.Pages.Account.Roles
 {
@@ -8,17 +10,36 @@ namespace ServiceHost.Areas.Administration.Pages.Account.Roles
     {
         [BindProperty]
         public EditRole Role { get; set; }
+        public List<SelectListItem> Permissions;
 
         private readonly IRoleApplication _roleApplication;
+        private readonly IEnumerable<IPermissionExposer> _exposers;
 
-        public EditModel(IRoleApplication roleApplication)
+        public EditModel(IRoleApplication roleApplication, IEnumerable<IPermissionExposer> exposers)
         {
             _roleApplication = roleApplication;
+            _exposers = exposers;
         }
 
         public void OnGet(long id)
         {
             Role = _roleApplication.GetDetails(id);
+            var Permissions = new List<PermissionDto>();
+            foreach (var exposer in _exposers)
+            {
+                var exposedPermissions = exposer.Expose();
+                foreach (var (key, value) in exposedPermissions)
+                {
+                    Permissions.AddRange(value);
+                    foreach (var permission in value)
+                    {
+                        var items = new SelectListItem(permission.Name, permission.Code.ToString()) 
+                        { 
+                            Group = new SelectListGroup { Name = key }
+                        };
+                    }
+                }
+            }
         }
 
         public IActionResult OnPost()
